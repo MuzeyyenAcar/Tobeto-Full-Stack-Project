76c97246f805f10d3b54323c8e5db4da5f1af833
ProgramciDusunur <lerenarazl@gmail.com>
2024-03-11 18:30:55 +0300

    init back-end base

---
+++ back-end/src/main/java/com/tobeto/TobetoSunumProjesiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tobeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobetoSunumProjesiBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobetoSunumProjesiBackEndApplication.class, args);
+	}
+
+}
